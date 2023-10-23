93e87b0dab6aa7c61502b369a29110fec6a5fd74
Dora <dlxiao@andrew.cmu.edu>
2023-10-22 21:56:02 -0400

    added a bunch of characters to character section

--- maobi/Views/ContentView.swift
+++ maobi/Views/ContentView.swift
@@ -4,7 +4,7 @@ import WebKit
 struct ContentView: View {
   var characterData = loadCharacterData()
   var basicStrokes = ["一", "丨", " ` ", "亅", "丶", "丿", "ノ"]
-  var sampleCharacters = ["小"]
+  var sampleCharacters = ["大", "小", "水", "天", "王", "十", "九", "八", "七", "六"]
   
   var body: some View {
     
