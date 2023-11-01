944ad71e4853f1e3828ed2ff93405c14eb7c9c4f
teresa-y <91036273+teresa-y@users.noreply.github.com>
2023-11-01 18:44:52 -0400

    laid out placeholder buttons with placeholder image for home screen

--- maobi/Views/HomeView.swift
+++ maobi/Views/HomeView.swift
@@ -13,8 +13,57 @@ struct HomeView: View {
     // Text(user.getPassword())
     // Text(String(user.getTotalStars()))
     
-    var html = levels.example()
-    WebView(html: html).frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+//    var html = levels.example()
+//    WebView(html: html).frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+      VStack{
+          
+          
+          HStack{
+              Button(action:{}){
+                  Image(systemName: "swift")
+                      .padding(10.0)
+                  
+              }
+              .frame(width: 162, height: 162)
+              .background(Color(red: 0.97, green: 0.94, blue: 0.91))
+              .cornerRadius(10)
+              .shadow(color: Color(red: 0.87, green: 0.78, blue: 0.7), radius: 0, x: 0, y: 4)
+
+              Button(action:{}){
+                  Image(systemName: "swift")
+                      .padding(10.0)
+                  
+              }
+              .frame(width: 162, height: 162)
+              .background(Color(red: 0.97, green: 0.94, blue: 0.91))
+              .cornerRadius(10)
+              .shadow(color: Color(red: 0.87, green: 0.78, blue: 0.7), radius: 0, x: 0, y: 4)
+          }
+                    
+          HStack{
+              Button(action:{}){
+                  Image(systemName: "swift")
+                      .padding(10.0)
+                  
+              }
+              .frame(width: 162, height: 162)
+              .background(Color(red: 0.97, green: 0.94, blue: 0.91))
+              .cornerRadius(10)
+              .shadow(color: Color(red: 0.87, green: 0.78, blue: 0.7), radius: 0, x: 0, y: 4)
+
+              Button(action:{}){
+                  Image(systemName: "swift")
+                      .padding(10.0)
+                  
+              }
+              .frame(width: 162, height: 162)
+              .background(Color(red: 0.97, green: 0.94, blue: 0.91))
+              .cornerRadius(10)
+              .shadow(color: Color(red: 0.87, green: 0.78, blue: 0.7), radius: 0, x: 0, y: 4)
+
+          }
+      }
+
     
   }
 }
