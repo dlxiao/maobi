5ed476fbdb3070f892f82e5c329938bc3c482854
teresa-y <91036273+teresa-y@users.noreply.github.com>
2023-11-02 18:04:21 -0400

    click through tutorial working

--- maobi/Views/TutorialView.swift
+++ maobi/Views/TutorialView.swift
@@ -9,38 +9,122 @@ import SwiftUI
 
 struct TutorialView: View {
     @State private var step = 0
+    @State private var isTutorialDone = false
     
     var body: some View {
         ZStack{
             HomeView().disabled(true)
             
-            ZStack{
-                Color.black.opacity(0.5).ignoresSafeArea()
-                Circle()
-                    .frame(width: 200, height: 200)
-                    .blendMode(.destinationOut)
-                    .offset(x: -80, y: -110)
+            if step == 0{
+                
+                ZStack{
+                    Color.black.opacity(0.6).ignoresSafeArea()
+                    Circle()
+                        .frame(width: 200, height: 200)
+                        .blendMode(.destinationOut)
+                        .offset(x: -80, y: -110)
+                    
+                }
+                .compositingGroup()
+                Text("Learn and practice the 8 basic strokes")
+                    .foregroundColor(Color.white)
+                    .frame(width: 200)
+                    .offset(x: -80, y: -250)
+            } else if step == 1 {
+                ZStack{
+                    Color.black.opacity(0.6).ignoresSafeArea()
+                    Circle()
+                        .frame(width: 200, height: 200)
+                        .blendMode(.destinationOut)
+                        .offset(x: 80, y: -110)
+                    
+                }
+                .compositingGroup()
+                Text("Learn stroke order and practice basic characters")
+                    .foregroundColor(Color.white)
+                    .frame(width: 200)
+                    .offset(x: 80, y: -250)
+
+            } else if step == 2 {
+                ZStack{
+                    Color.black.opacity(0.6).ignoresSafeArea()
+                    Circle()
+                        .frame(width: 200, height: 200)
+                        .blendMode(.destinationOut)
+                        .offset(x: -80, y: 140)
+                    
+                }
+                .compositingGroup()
+                Text("Learn and practice characters of a specific topic")
+                
+                    .foregroundColor(Color.white)
+                    .frame(width: 200)
+                    .offset(x: -80, y: 10)
+            } else if step == 3 {
+                ZStack{
+                    Color.black.opacity(0.6).ignoresSafeArea()
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .blendMode(.destinationOut)
+                        .offset(x: 170, y: -370)
+                    
+                }
+                .compositingGroup()
+                Text("Earn stars through practicing! Stars can unlock more characters")
+                
+                    .foregroundColor(Color.white)
+                    .frame(width: 150)
+                    .offset(x: 100, y: -270)
+
+            } else if step == 4 {
+                ZStack{
+                    Color.black.opacity(0.6).ignoresSafeArea()
+                    Circle()
+                        .frame(width: 200, height: 200)
+                        .blendMode(.destinationOut)
+                        .offset(x: 80, y: 140)
+                    
+                }
+                .compositingGroup()
+                Text("Unlock a character every day by doing a daily challenge!")
+                
+                    .foregroundColor(Color.white)
+                    .frame(width: 150)
+                    .offset(x: 80, y: 0)
+
+                
+            } else if step == 5 {
+                ZStack{
+                    Color.black.opacity(0.6).ignoresSafeArea()
+                    VStack{
+                        Text("You're all set.").font(.largeTitle).fontWeight(.bold).foregroundColor(.white).padding()
+                        Text("Have fun!").font(.largeTitle).fontWeight(.bold).foregroundColor(.white).padding()
+                    }
+                }
+
+            } else if step > 5{
+                HomeView().disabled(false)
                 
             }
-            .compositingGroup()
-            Text("Learn and practice the 8 basic strokes")
-                .foregroundColor(Color.white)
-                .frame(width: 200)
-                .offset(x: -80, y: -250)
+            if isTutorialDone == false{
+                Button(action: {
+                    if step <= 5 {
+                        step += 1
+                    }
+                }){
+                    Text("Next").foregroundColor(.white)
+                }
+                .offset(x: 150, y: 400)
+                
+                Button(action: {
+                    if step >= 1 {
+                        step -= 1
+                    }
+                }){
+                    Text("Back").foregroundColor(.white)
+                }
+                .offset(x: -150, y: -300)
             
-            Button(action: {
-                if step < 2 {
-                    step += 1
-                }
-            }){
-                Text("Next").foregroundColor(.white)
-            }
-            Button(action: {
-                if step >= 1 {
-                    step -= 1
-                }
-            }){
-                Text("Back").foregroundColor(.white)
             }
 
 
