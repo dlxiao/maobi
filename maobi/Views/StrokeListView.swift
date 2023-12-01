947071e801f740f8783ec772a740994b9782a31a
teresa <tyang218@gmail.com>
2023-12-01 01:41:54 -0500

    sticky search bar + adjusted padding

--- maobi/Views/StrokeListView.swift
+++ maobi/Views/StrokeListView.swift
@@ -14,41 +14,43 @@ struct StrokeListView: View {
   var body: some View {
     TopBarView(stars: opData.user!.totalStars)
     ZStack(alignment: .top) {
-      // Back button
-      HStack {
-        Button(action: { opData.currView = opData.lastView.removeLast() }) {
-          HStack {
-            Image(systemName: "chevron.left")
-            Text("Back")
-          }.foregroundColor(.black).font(.title3).fontWeight(.bold)
-        }.frame(maxWidth: .infinity, alignment: .leading)
-      }.padding()
-      
-      // Levels tiles
-      ScrollView{
-        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]){
-          ForEach(opData.levels.getBasicStrokes()){ c in
-            VStack{
-              Button(action: {
-                opData.character = c
-                opData.lastView.append(.strokelist)
-                opData.currView = .level
-              }) {
-                Text(c.toString())
-                  .foregroundColor(Color.black)
-                  .font(.system(.largeTitle))
-                  .fontWeight(.bold)
+        VStack{
+            // Back button
+            HStack {
+                Button(action: { opData.currView = opData.lastView.removeLast() }) {
+                    HStack {
+                        Image(systemName: "chevron.left")
+                        Text("Back")
+                    }.foregroundColor(.black).font(.title3).fontWeight(.bold)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+            }.padding()
+            
+            // Levels tiles
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]){
+                    ForEach(opData.levels.getBasicStrokes()){ c in
+                        VStack{
+                            Button(action: {
+                                opData.character = c
+                                opData.lastView.append(.strokelist)
+                                opData.currView = .level
+                            }) {
+                                Text(c.toString())
+                                    .foregroundColor(Color.black)
+                                    .font(.system(.largeTitle))
+                                    .fontWeight(.bold)
+                                
+                            }.frame(width: screenWidth / 2.5, height: screenWidth / 2.5)
+                                .background(Color(red: 0.97, green: 0.94, blue: 0.91))
+                                .cornerRadius(10)
+                                .shadow(color: Color(red: 0.87, green: 0.78, blue: 0.7), radius: 0, x: 0, y: 4)
+                            Text(c.getPinyin()).bold()
+                        }
+                    }
+                }
                 
-              }.frame(width: screenWidth / 2.5, height: screenWidth / 2.5)
-                .background(Color(red: 0.97, green: 0.94, blue: 0.91))
-                .cornerRadius(10)
-                .shadow(color: Color(red: 0.87, green: 0.78, blue: 0.7), radius: 0, x: 0, y: 4)
-              Text(c.getPinyin()).bold()
             }
-          }
         }
-        
-      }.padding(.top, 60)
     }
     
     
