870ad646921144885f1211873f87ca3f66ac9221
Dora <dlxiao@andrew.cmu.edu>
2023-12-04 10:57:35 -0500

    tested fingerdraw and character/level

--- maobiTests/maobiTests.swift
+++ maobiTests/maobiTests.swift
@@ -13,6 +13,9 @@ import FirebaseFirestoreSwift
 
 
 final class maobiTests: XCTestCase {
+  let levels = Levels()
+  let characterStrings = ["二", "小", "心", "川", "十", "门", "八", "六"]
+  let basicStrokeStrings = ["一", "丨", " ` ", "亅", "丶", "丿", "ノ"]
   
   override func setUpWithError() throws {
     print("setup with error")
@@ -26,6 +29,7 @@ final class maobiTests: XCTestCase {
   
   override func setUp() {
     super.setUp()
+    
     print("finished setup")
   }
   
@@ -34,11 +38,70 @@ final class maobiTests: XCTestCase {
     print("finished teardown")
   }
   
-  func testCreateUser() {
-    //    let testuser = UserRepository("sampleusername_1", "password_1")
-    print("Testing")
-    //    XCTAssertEqual(0, 0, "testing")
-    print("Done testing")
+//  func testCreateUser() {
+//    //    let testuser = UserRepository("sampleusername_1", "password_1")
+//    print("Testing")
+//    //    XCTAssertEqual(0, 0, "testing")
+//    print("Done testing")
+//  }
+  
+  
+  func testCharactersAndFingerDraw() {
+    // Check we get correct basic strokes and character levels
+    let basicStrokes = self.levels.getBasicStrokes()
+    let characterLevels = self.levels.getCharacterLevels()
+    for c in basicStrokes {
+      let cString = c.toString()
+      XCTAssertTrue(self.basicStrokeStrings.contains(cString))
+      
+      // Check details of character data
+      let cData = levels.getCharacter(cString)
+      let definition = cData.getDefinition()
+      let pinyin = cData.getPinyin()
+      let levelHTML = cData.getLevelHTML()
+      
+      XCTAssertNotNil(definition)
+      XCTAssertNotNil(pinyin)
+      XCTAssertTrue(levelHTML.contains(cString) || levelHTML.contains("svg"))
+      if(cString == "心") {
+        XCTAssertEqual(definition, "heart; mind; soul")
+        XCTAssertEqual(pinyin, "xīn")
+      }
+    }
+    for c in characterLevels {
+      let cString = c.toString()
+      XCTAssertTrue(self.characterStrings.contains(cString))
+      
+      // Check details of character data
+      let cData = levels.getCharacter(cString)
+      let definition = cData.getDefinition()
+      let pinyin = cData.getPinyin()
+      let levelHTML = cData.getLevelHTML()
+      
+      XCTAssertNotNil(definition)
+      XCTAssertNotNil(pinyin)
+      XCTAssertTrue(levelHTML.contains(cString))
+      if(cString == "心") {
+        XCTAssertEqual(definition, "heart; mind; soul")
+        XCTAssertEqual(pinyin, "xīn")
+      }
+    }
   }
+  
+  // Instantiate a fingerDraw and check that its properties are stored correctly
+  // and check the HTML generated for the quiz contains the correct character.
+  func testFingerDraw() {
+    // test for every character in the app
+    for c in self.characterStrings {
+      var fingerDraw = FingerDraw(character: c, size: 500)
+      XCTAssertEqual(fingerDraw.character, c)
+      XCTAssertEqual(fingerDraw.size, 500)
+      let quiz = fingerDraw.getIndividualQuizHTML()
+      XCTAssertTrue(quiz.contains(c))
+    }
+  }
+  
+  
+  
   
 }
